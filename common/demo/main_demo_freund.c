6967ac57741de2b6b0716c60a6b5e0961403f1ad
Markus Bader <markus.bader@mx-robotics.com>
2020-05-04 09:31:45 +0200

    neuen namen hinzugefuehgt

--- common/demo/main_demo_freund.c
+++ common/demo/main_demo_freund.c
@@ -7,6 +7,7 @@ int main(int argc, char **argv) {
     struct Freund *head = NULL;
     head = add(head, "Thomas", 32);
     head = add(head, "Max", 43);
+    head = add(head, "Klaus", 44);
     print(head);
 	clear(head);
     return 0;
